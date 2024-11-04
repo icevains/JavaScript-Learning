3ce3118582f1b66c37c3906e56bb88195a0534bb
cmh <1531103421@qq.com>
2024-11-04 21:04:39 +0800

    第八章结束，很多抽象的理论知识

--- JavaScript Learning/08-Behind-the-Scenes/script.js
+++ JavaScript Learning/08-Behind-the-Scenes/script.js
@@ -56,3 +56,95 @@ const addArrow = (a, b) => {
   a + b;
 };
 */
+
+/*
+
+
+// console.log(this);
+
+//jonas调用calcAge,calcAge调用isMillenial，
+//所以isMillenial当中的this是指Age，Age没有year这个属性
+const jonas = {
+  firstName: 'Jonas',
+  year: 1991,
+  calcAge: function () {
+    // console.log(this);
+    console.log(2037 - this.year);
+
+    // Solution 1 用self变量保存this-->jonas
+    // const self = this;
+    // const isMillenial = function () {
+    //   console.log(self);
+    //   console.log(self.year >= 1981 && self.year <= 1996);
+    // };
+
+    // Solution2 使用箭头函数
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+    isMillenial();
+  },
+
+  greet: () => console.log(`Hey ${this.firstName}`),
+};
+
+// jonas.greet();
+jonas.calcAge();
+
+*/
+
+/* 值传递
+let age = 23;
+let oldAge = age;
+age = 31;
+console.log(age);
+console.log(oldAge);
+
+const me = {
+  name: 'jonas',
+  age: '30',
+};
+
+let friend = me;
+friend.age = 27;
+console.log('Friend', friend);
+console.log('Me', me);
+*/
+
+// 原型
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+console.log(lastName, oldLastName);
+
+// 引用类型
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+
+// const marriedJessica = jessica;
+// marriedJessica.lastName = 'Davis';
+
+// console.log('Before Married', jessica);
+// console.log('After Married', marriedJessica);
+
+// Copying objects
+const jessica2 = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+  family: ['Alice', 'Bob'],
+};
+
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Davis';
+
+console.log('Before Married', jessica);
+console.log('After Married', jessicaCopy);
+
+jessicaCopy.family.push('Mary', 'Lily');
+console.log(jessica2.family);
+console.log(jessicaCopy.family);
