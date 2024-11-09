28edd60b6445074cb557ef8b4ad6129be20547dd
cmh <1531103421@qq.com>
2024-11-09 15:11:52 +0800

    初始闭包

--- JavaScript Learning/10-Functions/script.js
+++ JavaScript Learning/10-Functions/script.js
@@ -1,1 +1,301 @@
 'use strict';
+
+/*
+const bookings = [];
+
+// Function的默认参数
+const createBooking = function (
+  flightNum,
+  numPassengers = 3,
+  price = 199 * numPassengers
+) {
+  //   numPassengers ||= 10;
+  //   price ||= 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LG123', 2, 800);
+createBooking('LA555', 5);
+
+createBooking('LA555', undefined, 600);
+*/
+
+/*
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: 24739479284,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+  if (passenger.passport === 24739479284) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport! ');
+  }
+};
+
+console.log(flight);
+console.log(jonas);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000000);
+};
+
+newPassport(jonas);
+// console.log(jonas);
+checkIn(flight, jonas);
+*/
+
+/*
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order Function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+transformer('JavaScript is the best!', upperFirstWord);
+console.log('-----------------------');
+transformer('JavaScript is the best!', oneWord);
+
+const high5 = function () {
+  console.log('🤟🤟🤟');
+};
+
+document.body.addEventListener('click', high5);
+
+['Jonas', 'Martha', 'Adam'].forEach(high5);
+*/
+
+// const greet = function (greeting) {
+//   return function (name) {
+//     console.log(`${greeting} ${name}`);
+//   };
+// };
+
+// const greet = (greeting) => {
+//   (name) => {
+//     console.log(`${greeting} ${name}`);
+//   };
+// };
+
+// const greeterHey = greet('Hey');
+// greeterHey('Jonas');
+// greeterHey('Steven');
+
+// greet('Hello')('Jonas');
+
+// Challenge
+// const greetArr = (greeting) => (name) => {
+//   console.log(`${greeting} ${name}`);
+// };
+
+// greetArr('Hi')('Jonas');
+
+/*
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LH',
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(239, 'Jonas Schmedtann');
+lufthansa.book(635, 'Jonas Smith');
+console.log(lufthansa);
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book;
+
+// Does NOT Work
+// book(23, 'Saral Wimml');
+
+// Call()方法 == Apply()，修改this指向
+book.call(eurowings, 23, 'Sarah Williams');
+// console.log(eurowings);
+
+book.call(lufthansa, 285, 'Micheal Jdn');
+// console.log(lufthansa);
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: [],
+};
+
+book.call(swiss, 888, 'CMH');
+// console.log(swiss);
+
+// Apply()，参数列表需要是数组
+const flightData = [999, 'George Cooper'];
+book.apply(swiss, flightData);
+
+// console.log(swiss);
+
+// Bind Method
+const bookEW = book.bind(eurowings);
+const bookLh = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+bookEW(444, 'Steven Williams');
+// console.log(eurowings);
+
+// With Event Listeners
+lufthansa.plane = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+  this.plane++;
+  console.log(this.plane);
+};
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application(预设参数)
+const addTax = (rate, value) => value + value * rate;
+// console.log(addTax(0.1, 200));
+
+// this设为null,rate设置为固定的0.23
+const addVat = addTax.bind(null, 0.23);
+console.log(addVat(200));
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+const addVat2 = addTaxRate(0.23);
+console.log(addVat2(100));
+console.log(addVat2(23));
+*/
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Let's build a simple poll app!
+
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
+
+Here are your tasks:
+
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+  1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+        What is your favourite programming language?
+        0: JavaScript
+        1: Python
+        2: Rust
+        3: C++
+        (Write option number)
+  
+  1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
+2. Call this method whenever the user clicks the "Answer poll" button.
+3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+
+HINT: Use many of the tools you learned about in this and the last section 😉
+
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
+
+BONUS TEST DATA 1: [5, 2, 3]
+BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+
+GOOD LUCK 😀
+*/
+
+/*
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+      )
+    );
+    console.log(answer);
+
+    typeof answer === 'number' &&
+      answer < this.options.length &&
+      this.answers[answer]++;
+
+    // console.log(this.answers);
+    this.displayResults();
+    this.displayResults('string');
+  },
+
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
+  },
+};
+
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
+// [5, 2, 3]
+// [(1, 5, 3, 9, 6, 1)];
+
+*/
+
+// const runOnce = function () {
+//   console.log('This will never run again!');
+// };
+
+// runOnce();
+
+// 闭包(closure):任何函数都有权限访问创造它的函数的变量环境
+// booker是由secureBooking创造的，所以booker才可以访问secureBooking中的passengerCount
+// closure 优先于 scope chain
+const secureBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = secureBooking();
+booker();
+booker();
+booker();
+console.dir(booker);
