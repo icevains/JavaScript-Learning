932e7154dbed914b1f7faa0519afee9bc2c81294
cmh <1531103421@qq.com>
2024-11-09 15:48:39 +0800

    第十章Functions学完，十一章是关于Array

--- JavaScript Learning/10-Functions/script.js
+++ JavaScript Learning/10-Functions/script.js
@@ -285,17 +285,75 @@ poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
 // 闭包(closure):任何函数都有权限访问创造它的函数的变量环境
 // booker是由secureBooking创造的，所以booker才可以访问secureBooking中的passengerCount
 // closure 优先于 scope chain
-const secureBooking = function () {
-  let passengerCount = 0;
+// const secureBooking = function () {
+//   let passengerCount = 0;
 
-  return function () {
-    passengerCount++;
-    console.log(`${passengerCount} passengers`);
+//   return function () {
+//     passengerCount++;
+//     console.log(`${passengerCount} passengers`);
+//   };
+// };
+
+// const booker = secureBooking();
+// booker();
+// booker();
+// booker();
+// console.dir(booker);
+
+let f;
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
   };
 };
 
-const booker = secureBooking();
-booker();
-booker();
-booker();
-console.dir(booker);
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+
+h();
+f();
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+boardPassengers(180, 3);
+
+// Coding Challenge #2
+
+/* 
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! 
+Take all the time you need. 
+Think about WHEN exactly the callback function is executed, 
+and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
